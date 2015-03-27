bc4ae6248381dbc5965ceaeb67ddde2376e7aea0
Uladzimir Havenchyk <havenchyk@gmail.com>
2015-03-27 20:46:27 +0300

    Test next.

--- src/scripts/components/sockets.js
+++ src/scripts/components/sockets.js
@@ -1,15 +1,20 @@
-var io = require('socket.io-client')
+var io = require('socket.io-client');
 
 var socket = io('http://localhost:3005');
 
 socket.on('connect', function () {
     alert('connected!')
-})
+});
 
 socket.on('event', function (data) {
     alert('event!')
-})
+});
+
+socket.on('next', function (data) {
+    alert('next!');
+    alert(JSON.stringify(data));
+});
 
 socket.on('disconnect', function () {
     alert('disconnected!')
-})
+});
