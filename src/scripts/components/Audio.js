cd5bbb1bcf0071db4a529d60e04f52d1d15e5537
Uladzimir Havenchyk <havenchyk@gmail.com>
2015-03-28 17:19:14 +0300

    Added basic audio support.

--- src/scripts/components/Audio.js
+++ src/scripts/components/Audio.js
@@ -7,12 +7,35 @@ require('styles/Audio.styl');
 class Audio extends React.Component {
   toggleAudioStream() {
     console.log('toggle audio stream!');
+
+    var audioEl = document.getElementById('live_audio');
+
+    var aCtx,
+      analyser,
+      microphone;
+
+    navigator.webkitGetUserMedia({audio:true},
+      function(stream) {
+        console.log(stream);
+
+        aCtx = new AudioContext();
+        analyser = aCtx.createAnalyser();
+        microphone = aCtx.createMediaStreamSource(stream);
+        microphone.connect(analyser);
+        analyser.connect(aCtx.destination);
+
+      },
+      () => {
+        console.warn("Error getting audio stream from getUserMedia")
+      }
+    );
   }
 
   render() {
     return (
       <div>
         <p>Content for Audio</p>
+        <audio id="live_audio" autoplay></audio>
         <button className="pure-button" onClick={this.toggleAudioStream}>Enable AUDIO STREAM!</button>
       </div>
     );
