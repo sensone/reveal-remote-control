7e4826d91f86d6495ea1d67837043c4f2a960a9e
sensone <sensone@MacBook-Pro-sensone.local>
2015-03-28 17:00:42 +0300

    listening notes

--- src/scripts/components/Notes.js
+++ src/scripts/components/Notes.js
@@ -1,18 +1,27 @@
 'use strict';
 
 const React = require('react/addons');
+let model = require('./model');
 
 require('styles/Notes.styl');
 
 class Notes extends React.Component {
+  constructor(props) {
+    super(model);
+    this.state = model.toJSON();
+
+    model.on('change', function(model) {
+      this.setState(model.toJSON());
+    }, this);
+  }
+  createMarkup() {
+    return {__html: this.state.notes};
+  }
   render() {
     return (
-      <div>
-        <p>Content for Notes</p>
-      </div>
+      <div dangerouslySetInnerHTML={this.createMarkup()} />
     );
   }
 }
 
 module.exports = Notes;
-
