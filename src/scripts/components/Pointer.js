c7790d37ad815cbeb87defaa2e9aee60390456b6
Uladzimir Havenchyk <havenchyk@gmail.com>
2015-03-29 14:38:03 +0300

    Added pointer and zoomer.

--- src/scripts/components/Pointer.js
+++ src/scripts/components/Pointer.js
@@ -1,13 +1,34 @@
 'use strict';
 
 const React = require('react/addons')
-  , Control = require('./SmallControl');
+  , Control = require('./SmallControl')
+  , emit = require('./sockets')
+  , injectTapEventPlugin = require("react-tap-event-plugin");
+
+injectTapEventPlugin();
 
 class Pointer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    React.initializeTouchEvents(true);
+  }
+  handleClick(e) {
+
+    let event = e.touches[0];
+
+    console.log(event);
+
+    emit('pointer', {x: event.clientX, y: event.clientY, bounds: e.target.getBoundingClientRect()});
+  }
   render() {
     return (
       <div>
-        <div className="pointer-wrapper">Content for Pointer</div>
+        <div className="pointer-wrapper">
+          <div className="canvas" onTouchStart={this.handleClick}>
+
+          </div>
+        </div>
         <Control size="small"/>
       </div>
     );
--- src/scripts/components/Zoom.js
+++ src/scripts/components/Zoom.js
@@ -1,13 +1,29 @@
 'use strict';
 
 const React = require('react/addons')
+  , emit = require('./sockets')
   , Control = require('./SmallControl');
 
 class Zoom extends React.Component {
+  constructor(opts) {
+    super(opts);
+
+    React.initializeTouchEvents(true);
+  }
+  handleClick(e) {
+
+    let event = e.touches[0];
+
+    console.log(event);
+
+    emit('zoom', {x: event.clientX, y: event.clientY, bounds: e.target.getBoundingClientRect()});
+  }
   render() {
     return (
       <div>
-        <div className="zoom-wrapper">Content for Zoom</div>
+        <div className="zoom-wrapper">
+          <div className="canvas" onTouchStart={this.handleClick}></div>
+        </div>
         <Control size="small"/>
       </div>
     );
