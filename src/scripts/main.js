e3de6aef70295714781287e6284eed6b4cbe7be4
Evgeni Korsunov <5345043@gmail.com>
2015-05-06 14:22:55 +0300

    push main.js

---
+++ src/scripts/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var RevealRemoteControllApp = require('./RevealRemoteControllApp')
+  , React = require('react')
+  , Router = require('react-router')
+  , { Route, NotFoundRoute} = Router
+  , Control = require('./components/Control')
+  , Pointer = require('./components/Pointer')
+  , Audio = require('./components/Audio')
+  , Zoom = require('./components/Zoom')
+  , Timer = require('./components/Timer')
+  , Notes = require('./components/Notes')
+  , session = require('./models/session')
+  , Select = require('./components/Select');
+
+var content = document.getElementById('content');
+
+var Routes = (
+  <Route handler={RevealRemoteControllApp} path="/">
+    // TODO: change NotFoundRoute to DefaultRoute
+    // and fix get params
+    <NotFoundRoute handler={Control} />
+
+    <Route name="control" handler={Control}/>
+    <Route name="pointer" handler={Pointer}/>
+    <Route name="select" handler={Select}/>
+    <Route name="audio" handler={Audio}/>
+    <Route name="notes" handler={Notes}/>
+    <Route name="zoom" handler={Zoom}/>
+  </Route>
+);
+
+Router.run(Routes , function (Handler, state) {
+  var getParams = state.params
+    , params;
+
+  if (getParams.splat) {
+    params = getParams.splat.split('/');
+    session.set({
+      id: params[0],
+      token: params[1]
+    });
+  }
+
+  React.render(<Handler/> , content);
+});
